4a4488c866d05068b71ff3c3877678778f2e341e
Lyakh-Sergey <me@ljah.ru>
2025-03-03 22:31:57 +0300

    Homework is done.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Author author = new Author("Лев", "Толстой");
+        System.out.println("author.nameAuthor = " + author.getNameAuthor());
+        System.out.println("author.surnameAuthor = " + author.getSurnameAuthor());
+        Author author1 = new Author("Михаил", "Булгаков");
+        System.out.println("author.nameAuthor1 = " + author1.getNameAuthor());
+        System.out.println("author.surnameAuthor1 = " + author1.getSurnameAuthor());
+        Book book = new Book("Война и мир", author, 1956);
+        System.out.println("book.bookName = " + book.getBookName());
+        System.out.println("book.author = " + book.getAuthor().getNameAuthor() + " " + book.getAuthor().getSurnameAuthor());
+        System.out.println("book.yearOfPublication = " + book.getYearOfPublication());
+        Book book1 = new Book("Собачье сердце", author1, 1984);
+        System.out.println("book1.bookName = " + book1.getBookName());
+        System.out.println("book1.author = " + book1.getAuthor().getNameAuthor() + " " + book1.getAuthor().getSurnameAuthor());
+        System.out.println("book1.yearOfPublication = " + book1.getYearOfPublication());
+        book.setYearOfPublication(1996);
+        System.out.println("book.getYearOfPublication() = " + book.getYearOfPublication());
     }
 }
